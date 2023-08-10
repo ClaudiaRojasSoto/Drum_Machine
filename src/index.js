c53f56f290ccd418e8e14207641360e0b69ca4c4
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-08-09 22:26:29 -0400

    Upload the json packages

--- src/index.js
+++ src/index.js
@@ -9,10 +9,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}> {/* Envuelve tu aplicación con el Provider */}
+    <Provider store={store}>
+      {' '}
+      {/* Envuelve tu aplicación con el Provider */}
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
